ed5b34b7594acf07cdb0c69855f682986f647bdf
MrJayGamerTTV <mr.j.gamer98@gmail.com>
2022-02-18 18:20:06 +0200

    Changed blip color

--- functions/functions_client.lua
+++ functions/functions_client.lua
@@ -45,7 +45,7 @@ function CreateBlip(coords)
 
 	SetBlipSprite(blip, 361)
 	SetBlipScale(blip, 0.6)
-	SetBlipColour(blip, 4)
+	SetBlipColour(blip, 47)
 	SetBlipDisplay(blip, 4)
 	SetBlipAsShortRange(blip, true)
 
